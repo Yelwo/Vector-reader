826e0c29448c43dc3caaae3f7c543ed90c26dc11
Wolwowicz Filip <176095@centrala.bzwbk>
2018-09-26 14:33:46 +0200

    Added argparser

--- reader/__main__.py
+++ reader/__main__.py
@@ -1,10 +1,20 @@
+import argparse
+
 from reader.reader_class import Reader
 
 
-def main(input_file=None, output_file=None):
-    reader = Reader(input_file, output_file)
+def main():
+    args = parse_arguments()
+    reader = Reader(args.input_file, args.output_file)
     reader.translate_vector()
 
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file", "-i", help="input file path", type=str)
+    parser.add_argument("--output_file", "-o", help="output file path", type=str)
+    args = parser.parse_args()
+    return args
 
 if __name__ == '__main__':
     main()
